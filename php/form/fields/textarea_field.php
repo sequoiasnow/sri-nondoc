12c7fe353c4823d8729072b96c63c71c3309dba5
Sequoia Snow <the1codemaster@gmail.com>
2016-01-07 10:50:08 -0800

    Improved textarea

--- php/form/fields/textarea_field.php
+++ php/form/fields/textarea_field.php
@@ -1,6 +1,7 @@
 <?php
 class TextareaField extends Field {
     public function __toString() {
+        if ( ! $this['value'] ) { $this['value'] = ''; }
         return "<div class=\"form-field field-type-textarea\">
             <div class=\"field-name\">
                 <span>{$this->getPublic('title')}</span>
@@ -8,7 +9,9 @@ class TextareaField extends Field {
             <div class=\"field-description\">
                 <span>{$this->getPublic('description')}</span>
             </div>
-            <textarea {$this->getAttrStr()} ></textarea>
+            <textarea {$this->getAttrStr()} >
+                {$this['value']}
+            </textarea>
         </div>";
     }
 }
