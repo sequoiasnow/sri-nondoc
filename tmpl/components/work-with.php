9853533979a2f0f5f138ca942941acd266d3dbd0
Sequoia Snow <the1codemaster@gmail.com>
2016-02-23 09:43:00 -0800

    Fixed work with sideways

--- tmpl/components/work-with.php
+++ tmpl/components/work-with.php
@@ -10,44 +10,40 @@
 
     </div>
 
-    <div class="content">
+    <?php
+    // Get all the icons for the work-with list.
+    $workWith = WorkWith::getAllInstances();
+    ?>
 
-        <?php
-        // Get all the icons for the work-with list.
-        $workWith = WorkWith::getAllInstances();
-        ?>
+    <section class="group-container content-extra-large">
 
-        <section class="group-container content-extra-large">
+        <?php foreach ( $workWith as $group ) : ?>
 
-            <?php foreach ( $workWith as $group ) : ?>
+            <article class="card">
+                <div class="card-content">
 
-                <article class="card">
-                    <div class="card-content">
+                    <div class="front">
+                        <div class="image-container">
+                            <img src="<?php print Image::get_path( $group->image ); ?>" />
+                        </div>
 
-                        <div class="front">
-                            <div class="image-container">
-                                <img src="<?php print Image::get_path( $group->image ); ?>" />
-                            </div>
+                        <div class="name">
+                            <p><?php print __($group->title); ?></p>
+                        </div>
+                    </div> <!-- .front -->
 
-                            <div class="name">
-                                <p><?php print __($group->title); ?></p>
-                            </div>
-                        </div> <!-- .front -->
+                    <div class="back">
+                        <div class="description">
+                            <p><?php print __($group->blurb); ?></p>
+                        </div>
 
-                        <div class="back">
-                            <div class="description">
-                                <p><?php print __($group->blurb); ?></p>
-                            </div>
+                        <a class="doc-link" href="<?php print $group->link; ?>"><?php print __('Go!'); ?></a>
+                    </div> <!-- .back -->
 
-                            <a class="doc-link" href="<?php print $group->link; ?>"><?php print __('Go!'); ?></a>
-                        </div> <!-- .back -->
+                </div> <!-- .card-content -->
 
-                    </div> <!-- .card-content -->
+        <?php endforeach; ?>
 
-            <?php endforeach; ?>
-
-        </section> <!-- .group-container -->
-
-    </div> <!-- .content -->
+    </section> <!-- .group-container -->
 
 </div> <!-- .page-section -->
